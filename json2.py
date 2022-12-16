feec664c50fe51b03b14b182b67f67f1849b0de6
HappyRoach <117393350+HappyRoach@users.noreply.github.com>
2022-12-16 23:46:00 +0300

    commit

--- json2.py
+++
@@ -1,6 +0,0 @@
-
-def get_users(who, id):
-    with open(f"D:/formula/users/{who}.json", 'r', encoding='utf-8') as file:
-        users_badjson = file.read()
-        file.close()
-        return json.loads(users_badjson)[id]
--- role.py
+++ role.py
@@ -1,5 +1,9 @@
-class role:
-    def __init__(self, name):
-        self.name = name
-        self.access = access
-    
+import json
+
+def get_json(mainkey, key):
+    with open('json/role.json', 'r', encoding='utf-8') as file:
+        Badjson = file.read()
+        file.close()
+    GoodJson = json.loads(Badjson)[mainkey]
+    return GoodJson[0].get(key)
+print(get_json("Admin", "setrole"))
