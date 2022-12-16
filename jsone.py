13864015fcedb38c30e7780ddb9fd12d307d3fad
HappyRoach <117393350+HappyRoach@users.noreply.github.com>
2022-12-16 23:51:36 +0300

    Update jsone.py

--- jsone.py
+++ jsone.py
@@ -1,9 +1,11 @@
 import json
 
-def get_json(mainkey, key):
+def get_user_json(mainkey, key):
     with open('json/role.json', 'r', encoding='utf-8') as file:
         Badjson = file.read()
         file.close()
     GoodJson = json.loads(Badjson)[mainkey]
     return GoodJson[0].get(key)
-print(get_json("Admin", "setrole"))
+
+def get_question_json(key):
+    return key
