138cc823052cfe47d9d6d94546b7600158a0a5fa
Foo0s <oraproavp@list.ru>
2022-12-16 19:50:38 +0300

    Thist push

---
+++ mane.py
@@ -0,0 +1,18 @@
+"""Дальше бога нет....
+   ссылка на бота -> http://t.me/ndflDok_bot"""
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+TOKEN = "5614187069:AAFxZNIR2tNpFLFWQ2IirgubkPBQNQzLMos"
+chatbot = Bot(token=TOKEN)
+dp = Dispatcher(chatbot)
+'''Начало бота - команда start.'''
+@dp.message_handler(commands=["start"])
+async def start_message(message: types.Message):
+    await message.reply("Привет, пиши мне свои сообщения!")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
