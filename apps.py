f0046e0c513887ea0e4ee93c683956cd7857039c
divanys <hlinakcurbag@gmail.com>
2022-12-17 02:06:47 +0300

    i all restart

--- apps.py
+++ apps.py
@@ -14,11 +14,24 @@ class Db:
         self.cur = self.connection.cursor()
 
     def insert_user(self, id, phone, site):
-
         self.cur.execute(
             """INSERT INTO users (id, phone, site) VALUES (%s, %s, %s)"""
             , (id, phone, site))
         return True
 
-    # def insert_memders(self):
-Db().insert_user(132, 73112331, 'vk')
+    def insert_members(self, id, password, role):
+        self.cur.execute(
+            """INSERT INTO members (id, password, role) VALUES (%s, %s, %s)"""
+            , (id, password, role))
+        return True
+
+    def insert_message(self, id, text, question):
+        self.cur.execute(
+            """INSERT INTO message (id, text, question) VALUE (%s, %s, %s)"""
+            , (id, text, question))
+        return True
+
+
+Db().insert_user(132, 73111, 'vk')
+Db().insert_members(1, '12345', 'admin')
+Db().insert_message(1, 'hello', 'help')
