dc58bbde0bafddace3c41817ea52696aa538ee80
divanys <hlinakcurbag@gmail.com>
2022-12-17 10:26:53 +0300

    add new f(): delete_user

--- apps.py
+++ apps.py
@@ -4,6 +4,7 @@ import config as setting
 
 
 class Db:
+    # Здесь мы передаём информацию о пользователе/админах/сообщениях от пользователя
     def __init__(self):
         self.connection = psycopg2.connect(user=setting.USER,
                                            password=setting.PASSWORD,
@@ -14,26 +15,39 @@ class Db:
         self.cur = self.connection.cursor()
 
     def insert_user(self, id, phone, site):
+
         self.cur.execute(
             """INSERT INTO users (id, phone, site) VALUES (%s, %s, %s);"""
-            , (id, phone, site,))
+            , (id, phone, site,)
+        )
         return True
 
     def insert_members(self, id, password, role):
         self.cur.execute(
             """INSERT INTO members (id, password, role) VALUES (%s, %s, %s);"""
-            , (id, password, role,))
+            , (id, password, role,)
+        )
         return True
 
-    def insert_message(self, id, text, question):
+    def insert_message(self, id, question):
         self.cur.execute(
-            """INSERT INTO message (id, text, question) VALUES (%s, %s, %s);"""
-            , (id, text, question,))
+            """INSERT INTO message (id, question) VALUES (%s, %s);"""
+            , (id, question,)
+        )
         return True
-    #
-    # def delete_user(self):
-    #
 
-# Db().insert_user(id=1332, phone=None, site='vk')
+
+    # Здесь мы можем удалить информацию о пользователе/админах/сообщениях от пользователя
+    def delete_user(self, id):
+        self.cur.execute(
+            """DELETE FROM users WHERE id=%s;""",
+            (id,)
+        )
+        return True
+
+
+
+Db().insert_user(id='13fsa32', phone=None, site='vk')
 # Db().insert_members(12, '12345', 'admin')
-# Db().insert_message(11, 'he4llo', 'help')
+# Db().insert_message(11, 'help')
+# Db().delete_user(id=912824014)
